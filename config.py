9114c8c49a6e4b0973b8a03ac413114759ae8df5
Andres Fredes <andres.hector.fredes@gmail.com>
2021-07-06 18:12:39 +0930

    basic GUI created

---
+++ config.py
@@ -0,0 +1,17 @@
+# Geometry constants
+WIN = {
+    "X":200,
+    "Y":200,
+    "WIDTH":800,
+    "HEIGHT":500
+}
+
+URL_LABEL = {
+    "X":20,
+    "Y":20
+}
+
+DL_BUTTON = {
+    "X":600,
+    "Y":400
+}
--- ytdl.py
+++ ytdl.py
@@ -1,4 +1,35 @@
-from PyQt5 import QtWidgets as Widgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 
+from config import *
 import sys
+
+class YTDL_Window(QMainWindow):
+    def __init__(self):
+        super(YTDL_Window, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(WIN["X"], WIN["Y"], WIN["WIDTH"], WIN["HEIGHT"])
+        self.setWindowTitle("YouTube downloader")
+
+        self.urlLabel = QLabel(self)
+        self.urlLabel.move(URL_LABEL["X"], URL_LABEL["Y"])
+        self.urlLabel.setText("Enter YouTube URLs below:")
+        self.urlLabel.adjustSize()
+
+        self.dl_button = QPushButton(self)
+        self.dl_button.move(DL_BUTTON["X"], DL_BUTTON["Y"])
+        self.dl_button.setText("Download")
+        self.dl_button.clicked.connect(self.download)
+
+    def download():
+        pass
+
+def window():
+    app = QApplication(sys.argv)
+    win = YTDL_Window()
+    win.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    window()
