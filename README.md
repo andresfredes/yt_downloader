e2eb86039f134c6713c8c888a6df3ca9ddeb5f82
Andres Fredes <andres.hector.fredes@gmail.com>
2021-07-19 12:11:35 +0930

    Added Licensing information in accordance with GPLv3 requirements

--- README.md
+++ README.md
@@ -44,3 +44,28 @@ YouTube downloader logic and CLI code.
 
 ## ytdl.py
 GUI main file
+
+Requirements:
+-------------
+Python 3.8+
+PyQt5
+Packages listed in requirements.txt
+
+License:
+--------
+Copyright 2021, Andres Fredes, <andres.hector.fredes@gmail.com>
+
+This file is part of yt_downloader.
+
+yt_downloader is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+yt_downloader is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with yt_downloader.  If not, see <https://www.gnu.org/licenses/>.
--- gui.py
+++ gui.py
@@ -1,3 +1,20 @@
+# Copyright 2021, Andres Fredes, <andres.hector.fredes@gmail.com>
+
+# This file is part of yt_downloader.
+ 
+#     yt_downloader is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     yt_downloader is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with yt_downloader.  If not, see <https://www.gnu.org/licenses/>.
+
 from PyQt5.QtWidgets import (QMainWindow, QLabel, QPushButton, QMessageBox,
     QLineEdit, QHBoxLayout, QVBoxLayout, QWidget, QFileDialog, QRadioButton,
     QButtonGroup)
